e122f876758131178501b7125f1813d6e469ae58
AATHIL.TA <cluelessidiot@users.noreply.github.com>
2020-06-27 20:25:28 +0530

    java script basics

---
+++ week2/javaScriptBasics.js
@@ -0,0 +1,18 @@
+// write your code here
+var x = 5;
+print(x)
+var img = new SimpleImage("rodger.png")
+print(img)
+print(img.getWidth())
+print(img.getHeight())
+print(img.getPixel(22, 30))
+print(img.getRed(22, 30))
+
+function square(x) {
+    return x * x;
+}
+print(square(15))
+    // getWidth
+    // getHeight
+    // getPixel
+    // getRed, getGreen, getBlue
