818ec3b97f7255c4c2a08d44b795ba469411bc4c
AATHIL.TA <cluelessidiot@users.noreply.github.com>
2020-07-04 14:35:38 +0530

    interactive webpage

---
+++ week3/interactive/buttton.js
@@ -0,0 +1,16 @@
+function button() {
+    var dd1 = document.getElementById("div1");
+    var dd2 = document.getElementById("div2");
+    dd1.className = "green"
+    dd1.innerHTML = "changed inner"
+    dd2.className = "green"
+
+    // alert('inside function');
+}
+
+function text() {
+    var dd1 = document.getElementById("div1");
+    var dd2 = document.getElementById("div2");
+    dd1.innerHTML = "121212222"
+    dd2.innerHTML = "wwwwwwwww"
+}
